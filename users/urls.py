146dc1e70e2a2b12b7520ff5fdaafde63e41d89e
Aarya <falconblaze90@gmail.com>
2024-04-06 03:57:40 +0530

    Profile/Update

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login/", auth_view.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_view.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("",views.landing, name="landing"),
-    path("home/", views.home, name='home')
+    path("home/", views.home, name='home'),
+    path("profile/", views.profile, name="profile"),
+    path("update/", views.update_profile, name="profile-update"),
 ]
